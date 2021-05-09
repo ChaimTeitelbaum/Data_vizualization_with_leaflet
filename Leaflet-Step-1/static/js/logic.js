aed59f5508ae41f81aa2632282d18bd82714afd3
Chaim Teitelbaum <47922870+ChaimTeitelbaum@users.noreply.github.com>
2021-05-09 18:33:41 -0500

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -92,7 +92,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
   L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng);
-    },
+    
+      
     style: styleInfo,
     onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
